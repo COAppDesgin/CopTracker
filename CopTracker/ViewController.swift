2485eba442ad8253eeb3517f2a1c68a6133892ce
Tyler Jordan Cagle <tjcagle@Tylers-MacBook-Pro-2.local>
2017-07-07 14:45:02 -0600

    Initial Commit

---
+++ CopTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CopTracker
+//
+//  Created by Tyler Jordan Cagle on 7/7/17.
+//  Copyright © 2017 COAppDesign. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
